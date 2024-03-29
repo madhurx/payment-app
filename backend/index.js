99815de2b10ff74d23ab5d21e1baaa0962f1aa67
Madhur Mittal <madhurmittal55@gmail.com>
2024-03-29 16:08:22 +0530

    feat: password hash

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
+const v1Router = require("./v1Routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", v1Router);
 
-app.listen(3000);
+app.listen(3000, function () {
+	console.log("Server started on port 3000");
+});
--- backend/v1Routes/userRoutes.js
+++ backend/v1Routes/userRoutes.js
@@ -1,8 +1,52 @@
 const express = require("express");
+const z = require("zod");
+const { User } = require("../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 const router = express.Router();
 
-router.post("/signup", function (req, res) {
-	res.send("POST request to the homepage");
+const signupSchema = z.object({
+	userName: z.string(),
+	password: z.string(),
+	firstName: z.string(),
+	lastName: z.string(),
+});
+router.post("/signup", async (req, res) => {
+	const body = req.body;
+	const { success } = signupSchema.safeParse(body);
+	if (!success) {
+		return res.status(400).json({
+			message: "All fields are required",
+		});
+	}
+
+	const existingUser = await User.findOne({
+		userName: req.body.userName,
+	});
+	if (existingUser) {
+		return res.status(400).json({
+			message: "User already exists",
+		});
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	const user = await User.create({
+		userName: req.body.userName,
+		password: hashedPassword,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+	});
+
+	const userId = user._id;
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+
+	res.status(200).send({
+		message: "User created successfully",
+		token,
+	});
 });
 
 module.exports = router;
