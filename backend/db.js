e998a976aa35b818d26f8acd0d568ee31570d039
Madhur Mittal <madhurmittal55@gmail.com>
2024-03-30 11:32:25 +0530

    new schema

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,48 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URL);
 
 const userSchema = mongoose.Schema({
-	userName: String,
-	password: String,
-	firstName: String,
-	lastName: String,
+	userName: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true,
+		minLength: 3,
+		maxLength: 20,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 6,
+	},
+	firstName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 30,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 30,
+	},
 });
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = { User };
+const accountSchema = new mongoose.Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	balance: {
+		type: Number,
+		required: true,
+	},
+});
+
+const Account = mongoose.model("Account", accountSchema);
+
+module.exports = { User, Account };
